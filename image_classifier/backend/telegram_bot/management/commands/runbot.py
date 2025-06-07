9d18a8a35433f1076879d0c79fef024361df6702
KROKS13kir <arbitr13adv@gmail.com>
2025-06-07 17:03:38 +0300

    Настройка телеграм

--- image_classifier/backend/telegram_bot/management/commands/runbot.py
+++ image_classifier/backend/telegram_bot/management/commands/runbot.py
@@ -9,9 +9,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         app = build_app()
         if settings.TELEGRAM_USE_WEBHOOK:
+            # TELEGRAM_WEBHOOK_PATH = "/telegram/webhook/"
+            url_path = settings.TELEGRAM_WEBHOOK_PATH.lstrip("/")  # -> "telegram/webhook/"
             app.run_webhook(
                 listen="0.0.0.0",
                 port=int(os.getenv("PORT", 8443)),
+                url_path=url_path,
                 webhook_url=f"{settings.TELEGRAM_WEBHOOK_HOST}{settings.TELEGRAM_WEBHOOK_PATH}"
             )
         else:
